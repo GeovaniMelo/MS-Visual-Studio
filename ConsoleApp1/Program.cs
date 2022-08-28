db83e1b47e03cec163fabaee3facce42356adb3c
Geovani2 <Geovani2@DESKTOP-F960L1L>
2022-08-28 14:22:01 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            FileStream fs = new FileStream(@"C:\\", FileMode.Open, FileAccess.Read);
+
+
+        }
+    }
+}
